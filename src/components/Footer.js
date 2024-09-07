5551fd0a3d76bdd4633369995820556ce7dbeb7e
jjain31 <jjain312002@gmail.com>
2024-09-08 00:03:37 +0530

    all files

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <footer className="footer bg-gray-600  pb-0">
             <div className="footer-content">
-                <p className='text-center pt-5'>© 2024 ProFoods. All rights reserved.</p>
+                <p className='text-center pt-5'>© 2024 Foods. All rights reserved.</p>
                 <p className="flex justify-center items-center ml-2 mr-3 pb-5">Follow us on:
                     <a className="px-2" href="https://twitter.com" target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-x-twitter"></i></a> |
                     <a className="px-2" href="https://facebook.com" target="_blank" rel="noopener noreferrer"> <i className="fa-brands fa-facebook"></i></a> |
